4a08444298ceacde9c1dd483831cc64cf57a827b
Max Horn <max@quendi.de>
2011-08-30 23:58:45 +0100

    Added Smart Greet (TM), an advanced interactive greeting system
    Also added a NEWS file

--- greeter.c
+++ greeter.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+	char name[256];
+	printf("Please enter your name: ");
+	fgets(name, sizeof(name), stdin);
+
 	printf("Hello, wurld!\n");
+	printf("Hello, %s!\n", name);
 	return 0;
 }
