5743ea0b9f578d0a3110c5ae8fba06ce942f26c8
Max Horn <mhorn@tu-bs.de>
2011-08-31 18:30:22 +0200

    Remove trailing newline on name, only print a name if there is one.

--- greeter.c
+++ greeter.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 	char name[256];
+	int n;
 	printf("Please enter your name: ");
 	fgets(name, sizeof(name), stdin);
+	n = strlen(name);
+	if (n > 0 && name[n-1] == '\n')
+		name[--n] = 0;
 
 	printf("Hello, world!\n");
-	printf("Hello, %s!\n", name);
+	
+	if (n > 0)
+		printf("Hello, %s!\n", name);
 	return 0;
 }
